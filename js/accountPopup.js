f560fff16ccc3727ff56dd00f602ff8844c2e8b1
somethingwithagun <112172412+somethingwithagun@users.noreply.github.com>
2022-09-24 12:53:58 +0300

    delete2

--- js/accountPopup.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const accountPopup = document.getElementById("account-login");
-const accountButton = document.getElementById("account-content");
-function popupShow() {
-  accountPopup.classList.toggle("_active");
-}
-
